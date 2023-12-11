b85973d52af1094994f17b296e4f0a9701a91f21
Rick South <rick@natamus.com>
2023-12-12 00:03:06 +0100

    Spiders Produce Webs source code for 1.18.2.

---
+++ Forge/src/main/java/com/natamus/spidersproducewebs/forge/events/ForgeSpiderEvent.java
@@ -0,0 +1,25 @@
+package com.natamus.spidersproducewebs.forge.events;
+
+import com.natamus.spidersproducewebs.events.SpiderEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeSpiderEvent {
+	@SubscribeEvent
+	public void onPlayerTick(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.getCommandSenderWorld();
+		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+			return;
+		}
+
+		SpiderEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+}
