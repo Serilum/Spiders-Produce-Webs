a0f8a9b8180d1a47d28fbb5ee6130a9962e67109
Rick South <rick@natamus.com>
2023-12-12 00:03:21 +0100

    Spiders Produce Webs source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/spidersproducewebs/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.spidersproducewebs;
+
+import com.natamus.spidersproducewebs.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
