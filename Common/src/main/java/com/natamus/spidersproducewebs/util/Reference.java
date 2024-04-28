54a88f3f0d3c8b102fed7ebd5367239961f08093
Rick South <rick@natamus.com>
2024-04-28 18:57:16 +0200

    (v3.4) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- Common/src/main/java/com/natamus/spidersproducewebs/util/Reference.java
+++ Common/src/main/java/com/natamus/spidersproducewebs/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.spidersproducewebs.util;
 public class Reference {
 	public static final String MOD_ID = "spidersproducewebs";
 	public static final String NAME = "Spiders Produce Webs";
-	public static final String VERSION = "3.3";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.5]";
 }
