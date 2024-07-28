e064d7693d72000685d244982fe9c3e23b1e1b6d
Rick South <rick@natamus.com>
2024-07-28 07:14:09 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/spidersproducewebs/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/spidersproducewebs/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.spidersproducewebs;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.spidersproducewebs.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.spidersproducewebs.neoforge.events.NeoForgeSpiderEvent;
 import com.natamus.spidersproducewebs.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
