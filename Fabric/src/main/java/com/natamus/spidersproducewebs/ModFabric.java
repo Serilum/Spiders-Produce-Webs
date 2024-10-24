8a2ffa20b4b60dc5612c0bee4e74fe391f9c2cd8
Rick South <rick@natamus.com>
2024-10-24 18:00:47 +0200

    Spiders Produce Webs source code for 1.21.2.

---
+++ Fabric/src/main/java/com/natamus/spidersproducewebs/ModFabric.java
@@ -0,0 +1,37 @@
+package com.natamus.spidersproducewebs;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
+import com.natamus.spidersproducewebs.events.SpiderEvent;
+import com.natamus.spidersproducewebs.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectivePlayerEvents.PLAYER_TICK.register((ServerLevel world, ServerPlayer player) -> {
+			SpiderEvent.onPlayerTick(world, player);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
